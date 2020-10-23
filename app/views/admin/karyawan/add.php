46fd3e494004bdd76d41442a4f2bc4153226b3b1
rafi-zadanly <32492835+rafi-zadanly@users.noreply.github.com>
2020-10-23 15:50:09 +0700

    update

--- app/views/admin/karyawan/add.php
+++ app/views/admin/karyawan/add.php
@@ -3,33 +3,39 @@
         <form action="<?= BASEURL ?>/karyawan/store" method="post">
             <div class="card shadow mb-4 mt-4">
                 <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-light shadow-sm">
+                    <ol class="breadcrumb bg-light">
                         <li class="breadcrumb-item"><a href="<?= BASEURL ?>/karyawan">Karyawan</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Add</li>
                     </ol>
                 </nav>
                 <div class="card-body pt-2"> 
-                    <div class="">
-                        <span>Nama Lengkap :</span>
-                        <input type="text" name="name" class="form-control mt-2" value="<?= OlderValues::get("name") ?>" autofocus>
+                    <div class="row">
+                        <div class="col-6">
+                            <span>Nama Lengkap :</span>
+                            <input type="text" name="name" class="form-control mt-2" value="<?= OlderValues::get("name") ?>" autofocus>
+                        </div>
+                        <div class="col-6">
+                            <span>Email :</span>
+                            <input type="text" name="email" class="form-control mt-2" value="<?= OlderValues::get("email") ?>">
+                        </div>
+                        <div class="col-6 mt-3">
+                            <span>Pin :</span>
+                            <input type="text" name="pin" class="form-control mt-2" value="<?= OlderValues::get("pin") ?>">
+                        </div>
+                        <div class="col-6 mt-3">
+                            <?php $role = OlderValues::get("role"); ?>
+                            <span>Role :</span>
+                            <select name="role" class="form-control mt-2">
+                                <option value="">Pilih role</option>
+                                <option value="Admin" <?= $role == "Admin" ? "selected" : "" ?>>Admin</option>
+                                <option value="User" <?= $role == "User" ? "selected" : "" ?>>Karyawan</option>
+                            </select>                    
+                        </div>
                     </div>
-                    <div class="mt-3">
-                        <span>Email :</span>
-                        <input type="text" name="email" class="form-control mt-2" value="<?= OlderValues::get("email") ?>">
-                    </div>
-                    <div class="mt-3">
-                        <span>Pin :</span>
-                        <input type="text" name="pin" class="form-control mt-2" value="<?= OlderValues::get("pin") ?>">
-                    </div>
-                    <div class="mt-3">
-                        <?php $role = OlderValues::get("role"); ?>
-                        <span>Role :</span>
-                        <select name="role" class="form-control mt-2">
-                            <option value="">Pilih role</option>
-                            <option value="Admin" <?= $role == "Admin" ? "selected" : "" ?>>Admin</option>
-                            <option value="User" <?= $role == "User" ? "selected" : "" ?>>Karyawan</option>
-                        </select>                    
-                    </div>
+                    
+                    
+                    
+                    
                 </div>
                 <div class="card-footer text-right">
                     <a href="<?= BASEURL ?>/karyawan" class="btn btn-secondary">Kembali</a>
