942d391c373143365cf4413cceaa5c200ede7e4b
Fatimah Rizkyana Nuraini <fatimahrizkyananuraini@gmail.com>
2020-10-26 10:46:33 +0700

    update

---
+++ app/views/admin/absensi/detail.php
@@ -0,0 +1,53 @@
+<div class="card shadow mb-4">
+
+    <div class="card-header py-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-light">
+                <li class="breadcrumb-item"><a href="<?= BASEURL ?>/absensi">Absensi</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Edit</li>
+            </ol>
+        </nav>
+    </div>
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered text-center datatable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Lengkap</th>
+                        <th>Jam Datang</th>
+                        <th>Lama Bekerja</th>                        
+                    </tr>
+                </thead>                
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Fatimah Rizkyana Nuraini</td>
+                        <td>08:20:21</td>
+                        <td>17:26:23</td>                        
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Rafi Zadanly</td>
+                        <td>08:21:21</td>
+                        <td>17:22:29</td>                        
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Mikha T</td>
+                        <td>08:26:23</td>
+                        <td>17:28:23</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Asya Rasyanti</td>
+                        <td>08:10:19</td>
+                        <td>17:10:30</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
