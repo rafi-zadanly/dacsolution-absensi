8f515394de27455e140fac9c1b7fb604e052f996
Fatimah Rizkyana Nuraini <fatimahrizkyananuraini@gmail.com>
2020-10-27 09:25:10 +0700

    update

--- app/views/admin/absensi/index.php
+++ app/views/admin/absensi/index.php
@@ -5,14 +5,7 @@
     <div class="card-header py-3">
         <form action="<?= BASEURL ?>/absensi/index" method="post">
             <div class="row">
-                <div class="col-12">
-                    <a href="<?= BASEURL ?>/absensi/create_all" class="btn btn-primary">
-                        <i class="fas fa-plus mr-2"></i>Siapkan absensi hari ini
-                    </a>
-                    <a href="<?= BASEURL ?>/absensi/index/refresh" class="btn btn-outline-primary ml-3">
-                        <i class="fas fa-sync mr-2"></i>Muat ulang halaman
-                    </a>
-                </div>
+                
             </div>
             <div class="row mt-3">
                 <div class="col-3">
@@ -23,10 +16,18 @@
                     <label for="">Sampai tanggal :</label>
                     <input type="date" name="to" id="" class="form-control" value="<?= OlderValues::getWithoutUnset('absensi_date_to') ?>">
                 </div>
-                <div class="col-6 pt-3 mt-3">
+                <div class="col-2 pt-3 mt-3">
                     <button class="btn btn-primary" type="submit">
                         <i class="fas fa-search"></i>
                     </button>
+                </div>
+                <div class="col-4 pt-3 mt-3">
+                    <a href="<?= BASEURL ?>/absensi/create_all" class="btn btn-primary">
+                        <i class="fas fa-plus mr-2"></i>Siapkan absensi hari ini
+                    </a>
+                    <a href="<?= BASEURL ?>/absensi/index/refresh" class="btn btn-outline-primary ml-3">
+                        <i class="fas fa-sync"></i>
+                    </a>
                 </div>
             </div>
         </form>
